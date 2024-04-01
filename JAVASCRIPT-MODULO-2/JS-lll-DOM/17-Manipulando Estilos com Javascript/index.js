49c5cc421960469aed53e2e7500861d6d8169579
OrozinoGobira <orozino18@gmail.com>
2024-03-31 22:45:59 -0300

    modulo2

--- JAVASCRIPT-MODULO-2/JS-lll-DOM/17-Manipulando Estilos com Javascript/index.js
+++ JAVASCRIPT-MODULO-2/JS-lll-DOM/17-Manipulando Estilos com Javascript/index.js
@@ -1,5 +1,19 @@
-function useLightTheme(){
-  document.body.style.color = '#212529'
+function uselight(){
+  document.body.style.backgroundColor=' #f1f5f9'
+  document.body.style.color =' #212529'
 }
 
-document.getElementById('lightBtn').addEventListener('click', useLightTheme)
+function usedark (){
+  document.body.style.backgroundColor = '#212529'
+  document.body.style.color='#f1f5f9'
+}
+  
+function switchTheme(){
+  document.body.classList.toggle('is-light')
+  document.body.classList.toggle('is-dark')
+}
+
+
+document.getElementById('lightBtn').addEventListener('click', uselight)
+document.getElementById('darkBtn').addEventListener('click', usedark)
+document.getElementById('switchBtn').addEventListener('click',switchTheme)
