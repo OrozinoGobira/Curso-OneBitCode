c6618b5a8c3b35dc11539f5596c79e9d028f8218
OrozinoGobira <orozino18@gmail.com>
2024-07-08 22:14:24 -0300

    praticando

--- JAVASCRIPT/JS-lll-DOM/08-exercicio-11/index.js
+++ JAVASCRIPT/JS-lll-DOM/08-exercicio-11/index.js
@@ -1,41 +1,62 @@
-function addPlayer() {
-    const position = document.getElementById('position').value 
+function addPlayer(){
+  
+    const position = document.getElementById('position').value
     const name = document.getElementById('name').value
     const number = document.getElementById('number').value
-  
+    
     const confirmacao = confirm(
-        "Deseja escalar o jogador?\n" +
-         "Posição: "+ position + 
-         "\nNome: "+ name +
-         "\nCamisa: ("+ number + ")"
+        "Gostaria de escalar o jogador? "+ position + " "+ name + "("+ number + ")" 
     )
-
     if(confirmacao){
-    const teamList = document.getElementById('escalacao')  
-     const playerItem = document.createElement('li')
-     playerItem.id='player-'+ number
-     playerItem.innerText = position + " "+ name + " ("+ number + ")"
-     teamList.appendChild(playerItem)
+        const playerItem = document.getElementById('escalacao')
+        const player = document.createElement('li')
+        player.id = "player-"+ number
+        player.innerText = position + " "+ name + "("+ number + ")"
+        playerItem.appendChild(player)
 
-     document.getElementById('position').value = ''
-    document.getElementById('name').value = ''
-    document.getElementById('number').value = ''
+        document.getElementById('position').value = " "
+        document.getElementById('name').value = " "
+        document.getElementById('number').value = " "
     }
 }
 
-function removePlayer(){
-   const number = document.getElementById('numberToRemove').value
-   const playerToRemove = document.getElementById('player-'+ number)
+function removePlayer(){ 
+    const number = document.getElementById('numberToRemove').value
+    const playerToRemove = document.getElementById('player-'+number)
 
-   if(playerToRemove){
-    const confirmacao = confirm(
-        "Deseja remover o jogador?" + playerToRemove.innerText + "?"
-    )
-     if(confirmacao){
-        document.getElementById('escalacao').removeChild(playerToRemove)
-        document.getElementById('numberToRemove').value = ""
+    if(!playerToRemove){
+        alert("Não há jogadodor")
+    }else{
+     const confirmacao = confirm ("Deseja remover jogador? "+ playerToRemove.innerText + "?")
+    if(confirmacao){
+     document.getElementById('escalacao').removeChild(playerToRemove)
+     document.getElementById('numberToRemove').value = ""
+    
     }
-}else{
-        alert("Jogador não existe")
-   }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
